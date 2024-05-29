67c9b415b5246d735f4fba7de2fb206e071c7995
Alfie Gibson <alfie.gibson01@gmail.com>
2024-05-29 12:36:28 +0100

    Completed Task 5 - GET /api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const app = require("../app");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/development-data/index");
+const endpoints = require("../endpoints.json")
 
 beforeEach(() => {
     return seed(data);
@@ -18,6 +19,7 @@ beforeEach(() => {
         .get("/api/topics")
         .expect(200)
         .then(({ body }) => {
+          expect(body.topics.length).toBe(3);
               body.topics.forEach((topic) => {
                 expect(topic).toHaveProperty("slug");
                 expect(topic).toHaveProperty("description");
@@ -34,7 +36,7 @@ beforeEach(() => {
         .then(response => {
           expect(response.status).toBe(200);
           expect(response.type).toEqual("application/json");
-          expect(response.body).toEqual(require("../endpoints.json"));
+          expect(response.body).toEqual(endpoints);
         });
     });
   });
@@ -47,7 +49,7 @@ beforeEach(() => {
         .then(({ body }) => {
             expect(body.article).toHaveProperty("author");
             expect(body.article).toHaveProperty("title");
-            expect(body.article).toHaveProperty("article_id");
+            expect(body.article).toHaveProperty("article_id", 1);
             expect(body.article).toHaveProperty("body");
             expect(body.article).toHaveProperty("topic");
             expect(body.article).toHaveProperty("created_at");
@@ -58,11 +60,39 @@ beforeEach(() => {
     test("Responds with a 404 error if article ID does not exist", () => {
       return request(app)
       .get("/api/articles/999999")
-      .expect(404);
+      .expect(404)
+      .then(({ body }) => {
+        expect(body).toMatchObject({ "msg" : "No article found for article_id: 999999" });
+      });
     })
     test("Responds with a 400 error if article ID is invalid data", () => {
       return request(app)
       .get("/api/articles/ABC")
-      .expect(400);
+      .expect(400)
+      .then(({ body }) => {
+        expect(body).toMatchObject({ "msg" : "Bad Request" });
+      });
     })
     });
+  
+    describe("GET /api/articles", () => {
+      test("Responds with an article object with appropriate properties", () => {
+          return request(app)
+          .get("/api/articles")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.articles.length).toBe(36);
+            body.articles.forEach((article) => {
+              expect(article).toHaveProperty("author");
+              expect(article).toHaveProperty("title");
+              expect(article).toHaveProperty("article_id");
+              expect(article).toHaveProperty("topic");
+              expect(article).toHaveProperty("created_at");
+              expect(article).toHaveProperty("votes");
+              expect(article).toHaveProperty("article_img_url");
+              expect(article).toHaveProperty("comment_count");
+              expect(article).not.toHaveProperty("body");
+            })
+            })
+          });
+        })
